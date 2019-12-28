0949858b739ddd8bd15ca4d123898e06c741d65f
MinexAutomation <minex.automation@gmail.com>
2019-12-28 13:41:58 -0800

    Initial commit of source-stub.

---
+++ source/R5T.York.Base.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.York.Base.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
