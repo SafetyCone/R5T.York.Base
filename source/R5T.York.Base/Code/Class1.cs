40ddc34807b1d166d678391a786a9fb67e203bf1
MinexAutomation <minex.automation@gmail.com>
2019-12-28 13:43:33 -0800

    Added the IHttpJsonSource service definition interface type.

--- source/R5T.York.Base/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.York.Base
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.York.Base/Code/Services/Definitions/IHttpJsonSource.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.York.Base
+{
+    public interface IHttpJsonSource
+    {
+        T Get<T>(string jsonUrlEndpoint);
+    }
+}
